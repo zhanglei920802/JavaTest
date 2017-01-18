ff5b90cedc22400f26edc18bdef74bc40a076c71
zhanglei <zhanglei-y@oppo.com>
2017-01-18 11:24:49 +0800

    java并发编程学习

--- src/com/ray/java/net/jcip/examples/LazyInitRace.java
+++ src/com/ray/java/net/jcip/examples/LazyInitRace.java
@@ -2,9 +2,11 @@ package com.ray.java.net.jcip.examples;
 
 import net.jcip.annotations.*;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * LazyInitRace
- *
+ * <p>
  * Race condition in lazy initialization
  *
  * @author Brian Goetz and Tim Peierls
@@ -12,14 +14,44 @@ import net.jcip.annotations.*;
 
 @NotThreadSafe
 public class LazyInitRace {
-    private ExpensiveObject instance = null;
+    private static ExpensiveObject instance = null;
 
-    public ExpensiveObject getInstance() {
+    public synchronized static ExpensiveObject getInstance() {
         if (instance == null)
             instance = new ExpensiveObject();
         return instance;
     }
+
+    public static void main(String[] args) {
+        int count = 10000;
+        CountDownLatch countDownLatch = new CountDownLatch(count);
+        for (int i = 0; i < count; i++) {
+            new Thread(() -> {
+                getInstance();
+                countDownLatch.countDown();
+            }).start();
+        }
+
+        try {
+            countDownLatch.await();
+            System.out.println("instance count=" + ExpensiveObject.getInstanceCount());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
 
-class ExpensiveObject { }
+class ExpensiveObject {
+    static int sInstanceCount;
+
+    ExpensiveObject() {
+        super();
+        System.out.println("getInstance");
+        sInstanceCount++;
+    }
+
+    public static int getInstanceCount() {
+        return sInstanceCount;
+    }
+}
 
--- src/com/ray/java/net/jcip/examples/UnsafeSequence.java
+++ src/com/ray/java/net/jcip/examples/UnsafeSequence.java
@@ -1,6 +1,14 @@
 package com.ray.java.net.jcip.examples;
 
-import net.jcip.annotations.*;
+import com.ray.java.utils.Preconditions;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * UnsafeSequence
@@ -12,17 +20,37 @@ import net.jcip.annotations.*;
 public class UnsafeSequence {
     private int value;
 
+    //如果是线程安全的那么，两个list的size都是一样的
+    private static Set<Integer> resultSet = new HashSet<>();
+    private static List<Integer> resultList = new ArrayList<>();
+
+
     /**
      * Returns a unique value.
      */
-    public int getNext() {
+    public synchronized int getNext() {
         return value++;
     }
 
     public static void main(String[] args) {
         UnsafeSequence unsafeSequence = new UnsafeSequence();
+        CountDownLatch countDownLatch = new CountDownLatch(100);
         for (int i = 0; i < 100; i++) {
-            new Thread(() -> System.out.print(unsafeSequence.getNext()));
+            new Thread(() -> {
+                int next = unsafeSequence.getNext();
+                resultSet.add(next);
+                resultList.add(next);
+                System.out.println(next);
+                countDownLatch.countDown();
+            }).start();
+        }
+
+        try {
+            countDownLatch.await();
+            //如果没有加锁肯定会抛出异常,加锁就是正确的。测试结果也是这样
+            Preconditions.checkArgument(resultList.size() == resultSet.size());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
